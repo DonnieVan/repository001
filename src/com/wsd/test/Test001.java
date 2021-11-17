3b40bb5bfe126592fc13271d2e9ddacf5a8f2069
DonnieVan <360259440@qq.com>
2021-11-17 21:12:52 +0800

    第一次提交WebGitTest004

---
+++ src/com/wsd/test/Test001.java
@@ -0,0 +1,12 @@
+package com.wsd.test;
+
+/**
+ * @author: 凡玉丰
+ * @date: 2021/11/17
+ * @description:
+ */
+public class Test001 {
+    public static void main(String[] args) {
+        System.out.println("Web4test001");
+    }
+}
